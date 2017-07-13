6588d6032c9cbae97419c62efa955f197d9856dc
R. S. Doiel <rsdoiel@gmail.com>
2017-07-13 12:10:01 -0700

    Added demo of composite fields

---
+++ demos/hybrididx/demo.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ ! -d data ]; then
+    echo "missing data collection for demo"
+    exit 1
+fi
+
+# Index collection
+if [ -f index.bleve ]; then
+    rm -fR index.bleve
+fi
+
+echo "Indexing creating family_name, given_name, display_name fields via templates"
+dsindexer -c data idxdefn.json index.bleve
+
+# Show CSV output for indexes records
+dsfind -csv -size 100 -sort "orcid" -fields "orcid,family_name,given_name,display_name" -indexes index.bleve "*"
+
+
