754f2ed39dea844d217bbef455f138d0aa002ce8
R. S. Doiel <rsdoiel@gmail.com>
2017-08-11 14:04:05 -0700

    Quick Save

--- dataset.go
+++ dataset.go
@@ -41,7 +41,7 @@ import (
 
 const (
 	// Version of the dataset package
-	Version = "v0.0.3-rc3"
+	Version = "v0.0.3-rc4"
 
 	// License is a formatted from for dataset package based command line tools
 	License = `
