dad012e823cc93bbd82ac198961f910cfb871d39
R. S. Doiel <rsdoiel@gmail.com>
2017-11-03 14:00:44 -0700

    bumped version number

--- dataset.go
+++ dataset.go
@@ -41,7 +41,7 @@ import (
 
 const (
 	// Version of the dataset package
-	Version = "v0.0.5-dev2"
+	Version = "v0.0.6"
 
 	// License is a formatted from for dataset package based command line tools
 	License = `
