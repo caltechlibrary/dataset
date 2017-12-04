d2c32dc7ed986a1c844c884bdfc90546cca76e93
R. S. Doiel <rsdoiel@gmail.com>
2017-12-04 10:07:28 -0800

    fixed package-versions.bash bug

--- package-versions.bash
+++ package-versions.bash
@@ -4,15 +4,14 @@
 # Crawl the included Caltech Library package and display their versions in GOPATH
 #
 function crawl_code() {
-	grep "${1}" "$(findfile -s go .)" | cut -d \" -f 2 | sort -u | while read PNAME; do
-		echo -n "$PNAME -- "
-		V=$(grep 'Version = `' "$GOPATH/src/$PNAME/$(basename "$PNAME").go" | cut -d \` -f 2)
-		if [ "$V" = "" ]; then
-			echo "Unknown"
-		else
-			echo "$V"
+	findfile -s ".go" . | while read FNAME; do
+        grep "${1}" "${FNAME}" | cut -d \" -f 2 | while read PNAME; do
+		V=$(grep 'Version = `' "${GOPATH}/src/${PNAME}/$(basename "${PNAME}").go" | cut -d \` -f 2)
+		if [ "$V" != "" ]; then
+		    echo "$PNAME -- $V"
 		fi
-	done
+        done
+	done | sort -u
 }
 
 echo "The following package versions were used in this release"
