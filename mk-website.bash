055e354c3e109937ea4da19eb29bb28ba41b5bab
R. S. Doiel <rsdoiel@gmail.com>
2017-12-04 09:54:30 -0800

    cleanning up bash scripts

--- mk-website.bash
+++ mk-website.bash
@@ -1,62 +1,65 @@
 #!/bin/bash
 
 function cleanUpHTML() {
-    findfile -s ".html" . | while read P; do
-        rm "$P"
-    done
+	findfile -s ".html" . | while read P; do
+		rm "$P"
+	done
 }
 
 function FindNavMD() {
-    DNAME="$1"
-    if [ -f "${DNAME}/nav.md" ]; then
-        echo "${DNAME}/nav.md"
-        return
-    fi
-    DNAME=$(dirname "${DNAME}")
-    FindNavMD "${DNAME}"
+	DNAME="$1"
+	if [ -f "${DNAME}/nav.md" ]; then
+		echo "${DNAME}/nav.md"
+		return
+	fi
+	DNAME=$(dirname "${DNAME}")
+	FindNavMD "${DNAME}"
 }
 
 # Cleanup stale HTML files
 cleanUpHTML
 
 # Look through files and build new site
-mkpage "nav=nav.md" "content=markdown:$(cat LICENSE)" page.tmpl > license.html
+mkpage "nav=nav.md" "content=markdown:$(cat LICENSE)" page.tmpl >license.html
+git add license.html
 findfile -s ".md" . | while read P; do
-    DNAME=$(dirname "$P")
-    FNAME=$(basename "$P")
-    case "$FNAME" in
-        "README.md")
-        if [ ! -f "${DNAME}/index.md" ]; then
-            HTML_NAME="${DNAME}/index.html"
-        else
-            HTML_NAME="${DNAME}/README.html"
-        fi
-        ;;
-        *)
-        HTML_NAME=$(echo "$P" | sed -E 's/.md$/.html/g')
-        ;;
-    esac
-    if [ "${DNAME:0:4}" != "dist" ] && [ "${FNAME}" != "nav.md" ]; then
-        NAV=$(FindNavMD "$DNAME")
-        echo "Building $HTML_NAME from $DNAME/$FNAME and $NAV"
-        mkpage "nav=$NAV" "content=${DNAME}/${FNAME}" page.tmpl >"${HTML_NAME}"
-        git add "${HTML_NAME}"
-    else
-        echo "Skipping $P"
-    fi
+	DNAME=$(dirname "$P")
+	FNAME=$(basename "$P")
+	case "$FNAME" in
+	"README.md")
+		if [ ! -f "${DNAME}/index.md" ]; then
+			HTML_NAME="${DNAME}/index.html"
+		else
+			HTML_NAME="${DNAME}/README.html"
+		fi
+		;;
+	*)
+		HTML_NAME=$(echo "$P" | sed -E 's/.md$/.html/g')
+		;;
+	esac
+	if [ "${DNAME:0:4}" != "dist" ] && [ "${FNAME}" != "nav.md" ]; then
+		NAV=$(FindNavMD "$DNAME")
+		echo "Building $HTML_NAME from $DNAME/$FNAME and $NAV"
+		mkpage "nav=$NAV" "content=${DNAME}/${FNAME}" page.tmpl >"${HTML_NAME}"
+		git add "${HTML_NAME}"
+	else
+		echo "Skipping $P"
+	fi
 done
 
 # Cleanup some files we don't want to publish
 if [ -f "NOTES.html" ]; then
-    git rm -f "NOTES.html"
+	git rm -f "NOTES.html"
 fi
-if [ "DEVELOPMENT-NOTES.html" ]; then
-    git rm -f "DEVELOPMENT-NOTES.html"
+if [ -f "DEVELOPMENT-NOTES.html" ]; then
+	git rm -f "DEVELOPMENT-NOTES.html"
 fi
-if [ "README.html" ]; then
-    git rm -f "README.html"
+if [ -f "README.html" ]; then
+	git rm -f "README.html"
 fi
-if [ "TODO.html" ]; then
-    git rm -f "TODO.html"
+if [ -f "TODO.html" ]; then
+	git rm -f "TODO.html"
 fi
-
+if [ -f "RELEASES.html" ]; then
+	git rm -f "RELEASES.html"
+fi
--- package-versions.bash
+++ package-versions.bash
@@ -4,15 +4,15 @@
 # Crawl the included Caltech Library package and display their versions in GOPATH
 #
 function crawl_code() {
-grep "${1}" $(findfile -s go .) | cut -d \" -f 2 | sort -u | while read PNAME; do 
-    echo -n "$PNAME -- ";
-    V=$(grep 'Version = `' "$GOPATH/src/$PNAME/$(basename $PNAME).go" | cut -d \` -f 2)
-    if [ "$V" = "" ]; then
-        echo "Unknown"
-    else
-        echo "$V"
-    fi
-done
+	grep "${1}" "$(findfile -s go .)" | cut -d \" -f 2 | sort -u | while read PNAME; do
+		echo -n "$PNAME -- "
+		V=$(grep 'Version = `' "$GOPATH/src/$PNAME/$(basename "$PNAME").go" | cut -d \` -f 2)
+		if [ "$V" = "" ]; then
+			echo "Unknown"
+		else
+			echo "$V"
+		fi
+	done
 }
 
 echo "The following package versions were used in this release"
