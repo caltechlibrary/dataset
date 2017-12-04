d0e98b2a2b497a537e729af2368c7ea5d4d4b187
R. S. Doiel <rsdoiel@gmail.com>
2017-12-04 09:50:59 -0800

    updated package-versions.bash

--- package-versions.bash
+++ package-versions.bash
@@ -3,7 +3,8 @@
 #
 # Crawl the included Caltech Library package and display their versions in GOPATH
 #
-grep 'github.com/caltechlibrary/' *.go cmds/*/*.go | cut -d \" -f 2 | sort -u | while read PNAME; do 
+function crawl_code() {
+grep "${1}" $(findfile -s go .) | cut -d \" -f 2 | sort -u | while read PNAME; do 
     echo -n "$PNAME -- ";
     V=$(grep 'Version = `' "$GOPATH/src/$PNAME/$(basename $PNAME).go" | cut -d \` -f 2)
     if [ "$V" = "" ]; then
@@ -12,3 +13,8 @@ grep 'github.com/caltechlibrary/' *.go cmds/*/*.go | cut -d \" -f 2 | sort -u | 
         echo "$V"
     fi
 done
+}
+
+echo "The following package versions were used in this release"
+crawl_code "github.com/caltechlibrary/"
+crawl_code "github.com/rsdoiel/"
