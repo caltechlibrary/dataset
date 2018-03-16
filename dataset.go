15e66fd63a5a687891bd7103c52bbca8dd7ec55a
R. S. Doiel <rsdoiel@gmail.com>
2018-03-16 10:42:00 -0700

    prep for release

--- dataset.go
+++ dataset.go
@@ -43,7 +43,7 @@ import (
 
 const (
 	// Version of the dataset package
-	Version = `v0.0.36`
+	Version = `v0.0.37`
 
 	// License is a formatted from for dataset package based command line tools
 	License = `
