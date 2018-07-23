40b5f5b705bd7aab721ceab629385eb80285b7b6
R. S. Doiel <rsdoiel@gmail.com>
2018-07-23 14:21:30 -0700

    set version to v0.0.44 for pairtree support pre-release

--- dataset.go
+++ dataset.go
@@ -43,7 +43,7 @@ import (
 
 const (
 	// Version of the dataset package
-	Version = `v0.0.45`
+	Version = `v0.0.44`
 
 	// License is a formatted from for dataset package based command line tools
 	License = `
