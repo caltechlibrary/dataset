0c9ca281d3230a9da507da6c22d65d55aa676617
R. S. Doiel <rsdoiel@gmail.com>
2019-04-19 16:22:39 -0700

    Quick Save

--- py/make.bat
+++ py/make.bat
@@ -5,13 +5,13 @@ REM package the Python3 module from the Windows 10 command prompt.
 REM
 REM Requires: Go v1.23.4 or better 
 REM Miniconda Python 3.7 or better.
-REM and recent version of zip to the files up.
+REM Cygwin installed for gcc.
 REM
 
 
 REM python3 setup.py install --user --record files.txt
 
-go build -buildmode=c-shared -o "lib/libdataset.dll" "../libdataset/libdataset.go
+go build -buildmode=c-shared -o "libdataset/libdataset.dll" "../libdataset/libdataset.go
 python3 setup.py sdist
-echo "Check .\dist for tar ball or zip archives"
-
+echo "Check .\dist to see if this worked!"
+dir dist\
