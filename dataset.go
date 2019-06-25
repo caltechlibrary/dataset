42027679b76d8ef3ed3cb9b85c73f628accde3a8
R. S. Doiel <rsdoiel@gmail.com>
2019-06-25 11:44:31 -0700

    bumped version number for frame changes

--- dataset.go
+++ dataset.go
@@ -44,7 +44,7 @@ import (
 
 const (
 	// Version of the dataset package
-	Version = `v0.0.63`
+	Version = `v0.0.64`
 
 	// License is a formatted from for dataset package based command line tools
 	License = `
