8ae210e659436bbdea01584872e3c044a5aceee6
R. S. Doiel <rsdoiel@gmail.com>
2019-07-15 10:51:35 -0700

    bump patch level, fixed issue #95

--- dataset.go
+++ dataset.go
@@ -44,7 +44,7 @@ import (
 
 const (
 	// Version of the dataset package
-	Version = `v0.0.66`
+	Version = `v0.0.67`
 
 	// License is a formatted from for dataset package based command line tools
 	License = `
