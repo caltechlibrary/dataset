1fe4b68ecce2917b2e09d2b0899cf6f6796f10f0
R. S. Doiel <rsdoiel@gmail.com>
2020-02-07 09:55:37 -0800

    prep for pre-release v0.1.3

--- dataset.go
+++ dataset.go
@@ -47,7 +47,7 @@ import (
 
 const (
 	// Version of the dataset package
-	Version = `v0.1.2`
+	Version = `v0.1.3`
 
 	// License is a formatted from for dataset package based command line tools
 	License = `
