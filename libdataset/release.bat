0ad60b12e64a2329c2dff4d33d0d2d6d17f48fe4
R. S. Doiel <rsdoiel@gmail.com>
2020-03-11 13:47:30 -0700

    snapshot

--- libdataset/release.bat
+++ libdataset/release.bat
@@ -5,7 +5,7 @@ REM package the Python3 module from the Windows 10 command prompt.
 REM
 REM Requires: Go v1.23.4 or better 
 REM Miniconda Python 3.7 or better.
-REM Using conda: `conda install git` `conda install m2w64-gcc` `conda install tar gz`
+REM Using conda: `conda install git` `conda install m2w64-gcc` `conda install zip`
 REM
 @echo on
 go build -buildmode=c-shared -o "libdataset.dll" "..\libdataset\libdataset.go"
@@ -15,4 +15,4 @@ cd ..\dist
 copy ..\README.md .\
 copy ..\LICENSE .\
 copy ..\INSTALL.md .\
-tar zcvf libdataset-VERSION_NO-windows-amd64.tar.gz libdataset.dll README.md LICENSE INSTALL.md 
+zip "libdataset-%VERSION_NO%-windows-amd64.zip" libdataset.dll README.md LICENSE INSTALL.md 
