4135b76a38d93926683ff7fc5546613d5d787200
R. S. Doiel <rsdoiel@gmail.com>
2021-10-07 16:01:39 -0700

    Quick Save

--- datasetdDocs.go
+++ datasetdDocs.go
@@ -1,3 +1,16 @@
+package dataset
+
+import (
+	"fmt"
+)
+
+//
+// End point documentation for datasetd.go
+//
+
+// readmeDocuent() provides a copy of docs/datasetd.md
+func readmeDocument() string {
+	return fmt.Sprintf(`
 Datasetd
 ========
 
@@ -17,7 +30,7 @@ __datasetd__ can make one or more dataset collections visible over HTTP/HTTPS. T
 
 The "settings.json" file has the following structure
 
-```
+` + "```" + `
     {
         "host": "localhost:8483",
         "collections": {
@@ -31,7 +44,7 @@ The "settings.json" file has the following structure
             }
         }
     }
-```
+` + "```" + `
 
 In the "collections" object the "<COLLECTION_ID>" is a string which will be used as the start of the path in the URL. The "dataset" attribute sets the path to the dataset collection made available at "<COLLECTION_ID>". For each collection you can allow the following sub-paths, "create", "read", "update", "delete" and "keys". These sub-paths correspond to their counter parts in the dataset command line tool. By varying the settings of these you can support read only collections, drop off collections and function as a object store behind a web application.
 
@@ -71,7 +84,7 @@ E.g. if I have a settings file for "recipes" based on the collection
 "recipes.ds" and want to make it read only I would make the attribute
 "read" set to true and if I want the option of listing the keys in the collection I would set that true also.
 
-```
+` + "```" + `
 {
     "host": "localhost:8485",
     "collections": {
@@ -82,13 +95,13 @@ E.g. if I have a settings file for "recipes" based on the collection
         }
     }
 }
-```
+` + "```" + `
 
 I would start __datasetd__ with the following command line.
 
-```shell
+` + "```" + `shell
     datasetd settings.json
-```
+` + "```" + `
 
 This would display the start up message and log output of the service.
 
@@ -96,37 +109,37 @@ In another shell session I could then use cURL to list the keys and read
 a record. In this example I assume that "waffles" is a JSON document
 in dataset collection "recipes.ds".
 
-```shell
+` + "```" + `shell
     curl http://localhost:8485/recipies/read/waffles
-```
+` + "```" + `
 
 This would return the "waffles" JSON document or a 404 error if the 
 document was not found.
 
 Listing the keys for "recipes.ds" could be done with this cURL command.
 
-```shell
+` + "```" + `shell
     curl http://localhost:8485/recipies/keys
-```
+` + "```" + `
 
 This would return a list of keys, one per line. You could show
 all JSON documents in the collection be retrieving a list of keys
 and iterating over them using cURL. Here's a simple example in Bash.
 
-```shell
+` + "```" + `shell
     for KEY in $(curl http://localhost:8485/recipes/keys); do
        curl "http://localhost/8485/recipe/read/${KEY}"
     done
-```
+` + "```" + `
 
 Add a new JSON object to a collection.
 
-```shell
+` + "```" + `shell
     KEY="sunday"
     curl -X POST -H 'Content-Type:application/json' \
         "http://localhost/8485/recipe/create/${KEY}" \
      -d '{"ingredients":["banana","ice cream","chocalate syrup"]}'
-```
+` + "```" + `
 
 Documentation
 -------------
@@ -135,11 +148,71 @@ __datasetd__ provide documentation as plain text output via request
 to the service end points without parameters. Continuing with our
 "recipes" example. Try the following URLs with cURL.
 
-```
+` + "```" + `
     curl http://localhost:8485
     curl http://localhost:8485/recipes
     curl http://localhost:8485/recipes/read
-```
+` + "```" + `
 
 __datasetd__ is intended to be combined with other services like Solr 8.9.
 __datasetd__ only implements the simplest of object storage.
+`)
+}
+
+func keysDocument(collectionID string) string {
+	return fmt.Sprintf(`
+Retieve a JSON array of Keys from a collection.
+
+	"http://%s/%s/keys" accepts a "GET" HTTP method.
+
+`, config.Hostname, collectionID)
+}
+
+func createDocument(collectionID string) string {
+	return fmt.Sprintf(`
+Create a JSON document in the collection. Requires a unique key
+in the URL and the content most be JSON less than 1 MiB in size.
+
+	"http://%s/%s/created/<KEY>" requires a "POST" HTTP method
+
+Creates a JSON document for the <KEY> provided and HTTP 201 OK or 
+HTTP error if creation fails. The "POST" needs to be JSON encoded
+and using a Content-Type of "application/json"
+in the request header.
+
+`, config.Hostname, collectionID)
+}
+
+func readDocument(collectionID string) string {
+	return fmt.Sprintf(`
+Retrieve a JSON document from a collection.
+
+	"http://%s/%s/read/<KEY>" requires a "GET" HTTP method
+
+Returns the JSON document for given <KEY> or a HTTP error
+if not found.
+`, config.Hostname, collectionID)
+}
+
+func updateDocument(collectionID string) string {
+	return fmt.Sprintf(`
+Update a JSON document in a collection.
+
+    "http://%s/%s/updated/<KEY>" requires "POST" HTTP method
+
+Updates the JSON document the <KEY> provided and returns 
+HTTP 200 OK or HTTP error if update fails. The "POST" needs to 
+be JSON encoded and using a Content-Type of "application/json"
+in the request header.
+`, config.Hostname, collectionID)
+}
+
+func deleteDocument(collectionID string) string {
+	return fmt.Sprintf(`
+Deletes a JSON document from a collection.
+
+    "http://%s/%s/deleted/<KEY>" requires a "GET" HTTP method
+
+Returns HTTP 200 OK on successful deletion or an HTTP error otherwise.
+`, config.Hostname, collectionID)
+}
