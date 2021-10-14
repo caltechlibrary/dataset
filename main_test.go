1a038f1caef15591f41a5af248cfea032520caac
R. S. Doiel <rsdoiel@gmail.com>
2021-10-14 08:34:15 -0700

    Fix #113

--- main_test.go
+++ main_test.go
@@ -10,26 +10,11 @@ import (
 
 func TestMain(m *testing.M) {
 	appName := path.Base(os.Args[0])
-	settings := []byte(`
-{
-    "host": "localhost:8485",
-    "collections": {
-        "t1": {
-            "dataset": "t1.ds",
-            "keys": true,
-            "create": true,
-            "read": true,
-            "updated": true,
-            "delete": true
-        }
-    }
-}
-`)
 	dName := "testdata"
-	fName := path.Join(dName, "test-settings.json")
-	if _, err := os.Stat(dName); os.IsNotExist(err) {
-		os.MkdirAll("testdata", 0777)
+	if _, err := os.Stat(dName); os.IsNotExist(err) == false {
+		os.RemoveAll(dName)
 	}
+	os.MkdirAll("testdata", 0777)
 	cName := path.Join(dName, "t1.ds")
 	if _, err := os.Stat(cName); os.IsNotExist(err) {
 		if _, err := Init(cName); err != nil {
@@ -38,6 +23,25 @@ func TestMain(m *testing.M) {
 			os.Exit(1)
 		}
 	}
+	settings := []byte(fmt.Sprintf(`
+{
+    "host": "localhost:8485",
+    "collections": {
+        "t1": {
+            "dataset": "%s",
+            "keys": true,
+            "create": true,
+            "read": true,
+            "updated": true,
+            "delete": true,
+			"attach": true,
+			"retrieve": true,
+			"prune": true
+        }
+    }
+}
+`, cName))
+	fName := path.Join(dName, "test-settings.json")
 	if _, err := os.Stat(fName); os.IsNotExist(err) {
 		if err := ioutil.WriteFile(fName, settings, 0666); err != nil {
 			fmt.Fprintf(os.Stderr, "Cannot create %s\n", fName)
