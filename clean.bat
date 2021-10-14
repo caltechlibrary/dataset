476f1c019624a1edb2d047f7247ab84fd2898c57
R. S. Doiel <rsdoiel@gmail.com>
2021-10-14 10:16:28 -0700

    updated notes on compiling under Windows, added clean.bat

---
+++ clean.bat
@@ -0,0 +1,10 @@
+@echo off
+REM This is a Windows 10 Batch file for building dataset command
+REM from the command prompt.
+REM
+REM It requires: go version 1.12.4 or better and the cli for git installed
+REM
+DEL /S bin
+RMDIR /S bin
+DEL *.exe
+DEL libdataset\*.dll
