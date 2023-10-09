766f5f8df9710e0b2e295fc90143e3bab7eff940
R. S. Doiel <rsdoiel@gmail.com>
2023-10-09 07:54:51 -0700

    fix: made sure dsimpoter man page got generated

--- dsquery.1.md
+++ dsquery.1.md
@@ -1,4 +1,4 @@
-%dsquery(1) dataset user manual | version 2.1.8 46e7ae0
+%dsquery(1) dataset user manual | version 2.1.8 b048b3d
 % R. S. Doiel and Tom Morrell
 % 2023-10-09
 
--- version.go
+++ version.go
@@ -12,7 +12,7 @@ const (
     ReleaseDate = "2023-10-09"
 
     // ReleaseHash, the Git hash when version.go was generated
-    ReleaseHash = "46e7ae0"
+    ReleaseHash = "b048b3d"
 
     LicenseText = `
 Copyright (c) 2022, Caltech All rights not granted herein are expressly
